f296d5b7f89c4ac8b79463e0ff20c90a29b9c9a3
ukoranne <ukoranne@snaproute.com>
2016-08-05 15:48:00 -0700

    Ip address fix

--- collectdmon/collectd_linkstat_python.py
+++ collectdmon/collectd_linkstat_python.py
@@ -57,8 +57,8 @@ class PortMon(object):
     def read_callback(self):
         print("Read callback called")
         portstat = PortStat()
-        portout = portstat.get_portstats("10.1.10.242")
-        portcfg = portstat.get_portconfigs("10.1.10.242")
+        portout = portstat.get_portstats("localhost")
+        portcfg = portstat.get_portconfigs("localhost")
 	pmap = {}
 	for  pcfg in portcfg:
             pmap[json.dumps(pcfg["Object"]["IntfRef"])] = portstat.parse_speed(pcfg)
